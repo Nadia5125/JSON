3d91e0c2394ae43998a63082d852c16f4be03e9b
Nadia <nadiamiswiyati@gmail.com>
2019-12-10 13:39:25 -0500

    added

---
+++ index.js
@@ -0,0 +1,13 @@
+var thePets= [
+    {
+        "name": "Mellow",
+        "species": "Cat",
+        "food": "Fish",
+    },
+    {
+        "name": "Mile",
+        "species": "Dog",
+        "food": "Meat",
+    }
+
+];
